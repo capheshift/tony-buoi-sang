fdaba4f44bcc15535f5a9d7407c8773eeaaaaa32
Thongtran <12520423@gm.uit.edu.vn>
2015-09-07 20:25:09 +0700

    no message

--- app.js
+++ app.js
@@ -21412,7 +21412,7 @@
 	      React.DOM.div({className: "text-center"}, 
 	        React.DOM.nav({className: "navbar"}, 
 	          React.DOM.div({className: "header"}, 
-	            React.DOM.h1(null, "Tony Buổi sáng")
+	            React.DOM.h1(null, "Tony Buổi Sáng")
 	          )
 	        )
 	      )
